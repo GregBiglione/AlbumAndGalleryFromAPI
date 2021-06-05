b4830e74214c1b012f4f0fde2b6e62b37f6fab3d
GregBiglione <gregory.biglione81@gmail.com>
2021-06-05 06:48:40 +0200

    Added getAllAuthors() in RetrofitService

--- app/src/main/java/com/greg/albumandgalleryfromapi/api/RetrofitService.kt
+++ app/src/main/java/com/greg/albumandgalleryfromapi/api/RetrofitService.kt
@@ -1,86 +1,47 @@
 package com.greg.albumandgalleryfromapi.api
 
 import com.greg.albumandgalleryfromapi.constant.Constant.Companion.BASE_URL
+import com.greg.albumandgalleryfromapi.model.Album
+import com.greg.albumandgalleryfromapi.model.Author
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
-class RetrofitService {
+interface RetrofitService {
 
-    private var retrofitService: RetrofitService? = null
-    private lateinit var retrofit: Retrofit
+    @GET("albums")
+    fun getAllAlbums(): Call<List<Album>>
 
-    //----------------------------------------------------------------------------------------------
-    //-------------------------------- Initialize retrofit -----------------------------------------
-    //----------------------------------------------------------------------------------------------
+    @GET("users")
+    fun getAllAuthors(): Call<List<Author>>
 
-    fun getInstance() : RetrofitService{
-        if (retrofitService == null){
-            retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            getApi()
+    companion object{
+
+        private var retrofitService: RetrofitService? = null
+        private lateinit var retrofit: Retrofit
+
+        //------------------------------------------------------------------------------------------
+        //-------------------------------- Initialize retrofit -------------------------------------
+        //------------------------------------------------------------------------------------------
+
+        fun getInstance() : RetrofitService{
+            if (retrofitService == null){
+                retrofit = Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+                getApi()
+            }
+            return retrofitService!!
         }
-        return retrofitService!!
+
+        //------------------------------------------------------------------------------------------
+        //-------------------------------- Initialize JsonPlaceHolderApi ---------------------------
+        //------------------------------------------------------------------------------------------
+
+        private fun getApi() {
+            retrofitService = retrofit.create(RetrofitService::class.java)
+        }
     }
-
-    //----------------------------------------------------------------------------------------------
-    //-------------------------------- Initialize JsonPlaceHolderApi -------------------------------
-    //----------------------------------------------------------------------------------------------
-
-    fun getApi() : JsonPlaceHolderApi{
-        return retrofit.create(JsonPlaceHolderApi::class.java)
-    }
-
-    //var retrofitService: RetrofitService? = null
-//
-    //fun getInstance() : RetrofitService {
-//
-    //    if (retrofitService == null) {
-    //        val retrofit = Retrofit.Builder()
-    //            .baseUrl("https://howtodoandroid.com/")
-    //            .addConverterFactory(GsonConverterFactory.create())
-    //            .build()
-    //        retrofitService = retrofit.create(RetrofitService::class.java)
-    //    }
-    //    return retrofitService!!
-    //}
-    //private lateinit var retrofit: Retrofit
-    //private var instance: RetrofitClient? = null
-    ////private lateinit var jsonPlaceHolderApi: JsonPlaceHolderApi
-//
-    ////----------------------------------------------------------------------------------------------
-    ////-------------------------------- Initialize retrofit -----------------------------------------
-    ////----------------------------------------------------------------------------------------------
-//
-    //private fun RetrofitClient(): RetrofitClient? { //1
-    //    retrofit = Retrofit.Builder()
-    //        .baseUrl(BASE_URL)
-    //        .addConverterFactory(ScalarsConverterFactory.create())
-    //        .addConverterFactory(GsonConverterFactory.create())
-    //        .build()
-    //    return null //2
-    //}
-//
-//
-    ////----------------------------------------------------------------------------------------------
-    ////-------------------------------- Get instance ------------------------------------------------
-    ////----------------------------------------------------------------------------------------------
-//
-    //@Synchronized
-    //fun getInstance(): RetrofitClient?{
-    //    if (instance == null){
-    //        instance = RetrofitClient()
-    //    }
-    //    return instance
-    //}
-//
-    ////----------------------------------------------------------------------------------------------
-    ////-------------------------------- Initialize JsonPlaceHolderApi -------------------------------
-    ////----------------------------------------------------------------------------------------------
-//
-    //fun getApi(): JsonPlaceHolderApi {
-    //    return retrofit.create(JsonPlaceHolderApi::class.java)
-    //}
-
 }
