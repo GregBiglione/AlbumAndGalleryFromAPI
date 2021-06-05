e02add4092008792c150cd9bf0edecd2969a8f1e
GregBiglione <gregory.biglione81@gmail.com>
2021-06-05 07:10:22 +0200

    Added getAllAuthors() in MainViewModel

--- app/src/main/java/com/greg/albumandgalleryfromapi/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/greg/albumandgalleryfromapi/viewmodel/MainViewModel.kt
@@ -3,15 +3,23 @@ package com.greg.albumandgalleryfromapi.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.greg.albumandgalleryfromapi.model.Album
+import com.greg.albumandgalleryfromapi.model.Author
 import com.greg.albumandgalleryfromapi.repositories.AlbumRepository
+import com.greg.albumandgalleryfromapi.repositories.AuthorRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel(private val albumRepository: AlbumRepository): ViewModel() {
+class MainViewModel(private val albumRepository: AlbumRepository,
+                    private val authorRepository: AuthorRepository): ViewModel() {
 
     val albumList = MutableLiveData<List<Album>>()
+    val authorList = MutableLiveData<List<Author>>()
     val errorMessage = MutableLiveData<String>()
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Get all albums ----------------------------------------------
+    //----------------------------------------------------------------------------------------------
 
     fun getAllAlbums(){
         val response = albumRepository.getAllAlbums()
@@ -21,6 +29,23 @@ class MainViewModel(private val albumRepository: AlbumRepository): ViewModel() {
             }
 
             override fun onFailure(call: Call<List<Album>>, t: Throwable) {
+                errorMessage.postValue(t.message)
+            }
+        })
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Get all authors ---------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    fun getAllAuthors(){
+        val response = authorRepository.getAllAuthors()
+        response.enqueue(object : Callback<List<Author>>{
+            override fun onResponse(call: Call<List<Author>>, response: Response<List<Author>>) {
+                authorList.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Author>>, t: Throwable) {
                 errorMessage.postValue(t.message)
             }
         })
