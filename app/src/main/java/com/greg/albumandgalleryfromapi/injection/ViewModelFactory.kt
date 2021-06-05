73a7f7a0bf68cf79732db933655c924b02c272e0
GregBiglione <gregory.biglione81@gmail.com>
2021-06-05 07:13:27 +0200

    Added AuthorRepository in ViewModelFactory

--- app/src/main/java/com/greg/albumandgalleryfromapi/injection/ViewModelFactory.kt
+++ app/src/main/java/com/greg/albumandgalleryfromapi/injection/ViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.greg.albumandgalleryfromapi.injection
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.greg.albumandgalleryfromapi.repositories.AlbumRepository
+import com.greg.albumandgalleryfromapi.repositories.AuthorRepository
 import com.greg.albumandgalleryfromapi.viewmodel.MainViewModel
 import java.lang.IllegalArgumentException
 
-class ViewModelFactory(private val albumRepository: AlbumRepository): ViewModelProvider.Factory {
+class ViewModelFactory(private val albumRepository: AlbumRepository,
+                       private val authorRepository: AuthorRepository): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)){
-            return MainViewModel(albumRepository) as T
+            return MainViewModel(albumRepository, authorRepository) as T
         }
 
         throw IllegalArgumentException("Unknown View Model class")
