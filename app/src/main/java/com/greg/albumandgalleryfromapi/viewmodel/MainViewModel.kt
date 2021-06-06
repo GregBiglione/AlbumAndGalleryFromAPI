c8c12c7e3d90b9a648c167807d3bb511bb26969e
GregBiglione <gregory.biglione81@gmail.com>
2021-06-06 19:54:33 +0200

    Added getAllGalleries() in MainViewModel

--- app/src/main/java/com/greg/albumandgalleryfromapi/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/greg/albumandgalleryfromapi/viewmodel/MainViewModel.kt
@@ -4,17 +4,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.greg.albumandgalleryfromapi.model.Album
 import com.greg.albumandgalleryfromapi.model.Author
+import com.greg.albumandgalleryfromapi.model.Gallery
 import com.greg.albumandgalleryfromapi.repositories.AlbumRepository
 import com.greg.albumandgalleryfromapi.repositories.AuthorRepository
+import com.greg.albumandgalleryfromapi.repositories.GalleryRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainViewModel(private val albumRepository: AlbumRepository,
-                    private val authorRepository: AuthorRepository): ViewModel() {
+                    private val authorRepository: AuthorRepository,
+                    private val galleryRepository: GalleryRepository): ViewModel() {
 
     val albumList = MutableLiveData<List<Album>>()
     val authorList = MutableLiveData<List<Author>>()
+    val galleryList = MutableLiveData<List<Gallery>>()
     val errorMessage = MutableLiveData<String>()
 
     //----------------------------------------------------------------------------------------------
@@ -46,6 +50,23 @@ class MainViewModel(private val albumRepository: AlbumRepository,
             }
 
             override fun onFailure(call: Call<List<Author>>, t: Throwable) {
+                errorMessage.postValue(t.message)
+            }
+        })
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------------------- Get all photos ----------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    fun getAllGalleries(){
+        val response = galleryRepository.getAllGalleries()
+        response.enqueue(object : Callback<List<Gallery>>{
+            override fun onResponse(call: Call<List<Gallery>>, response: Response<List<Gallery>>) {
+                galleryList.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Gallery>>, t: Throwable) {
                 errorMessage.postValue(t.message)
             }
         })
