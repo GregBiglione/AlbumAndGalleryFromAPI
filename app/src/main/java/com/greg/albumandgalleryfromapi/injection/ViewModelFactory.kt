aade374f026b8aaf98d0367ea5857588e6e54a52
GregBiglione <gregory.biglione81@gmail.com>
2021-06-06 19:57:49 +0200

    Added GalleryRepository in ViewModelFactory

--- app/src/main/java/com/greg/albumandgalleryfromapi/injection/ViewModelFactory.kt
+++ app/src/main/java/com/greg/albumandgalleryfromapi/injection/ViewModelFactory.kt
@@ -4,14 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.greg.albumandgalleryfromapi.repositories.AlbumRepository
 import com.greg.albumandgalleryfromapi.repositories.AuthorRepository
+import com.greg.albumandgalleryfromapi.repositories.GalleryRepository
 import com.greg.albumandgalleryfromapi.viewmodel.MainViewModel
 import java.lang.IllegalArgumentException
 
 class ViewModelFactory(private val albumRepository: AlbumRepository,
-                       private val authorRepository: AuthorRepository): ViewModelProvider.Factory {
+                       private val authorRepository: AuthorRepository,
+                       private val galleryRepository: GalleryRepository): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)){
-            return MainViewModel(albumRepository, authorRepository) as T
+            return MainViewModel(albumRepository, authorRepository, galleryRepository) as T
         }
 
         throw IllegalArgumentException("Unknown View Model class")
